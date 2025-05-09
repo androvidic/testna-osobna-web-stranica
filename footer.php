5266aa1daa5b65f25220cb5e5ef5c1791a5f2ec4
androvidic <andrija.vidic3@gmail.com>
2025-05-09 13:40:31 +0200

    popravljen footer

--- footer.php
+++ footer.php
@@ -12,8 +12,8 @@
     <div class="footer-content" id="footer">
         <img src="slike/ANDRIJA%20VIDI%C4%86.jpg" alt="Developer je Andrija Vidić" title="Developer je Andrija Vidić" width="180" height="280"> 
         <p>Developer: Andrija Vidić</p>
-        <a href="mailto:andrija.vidic3@gmail.com">Mail:andrija.vidic3@gmail.com</a>
-        <a href="tel:+38516165165">Tel: 0993033672</a>
+        <p>Email:<a href="mailto:andrija.vidic3@gmail.com"> andrija.vidic3@gmail.com</a></p>
+        <p>Broj telefona:<a href="tel:+38516165165"> 0993033672</a></p>
         <p>Copyright 2025. New Breed Assist in digital</p>
     </div>
 </footer>
