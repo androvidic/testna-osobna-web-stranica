d874804ee221a18bc91e333e445a4322806ea168
androvidic <andrija.vidic3@gmail.com>
2025-05-12 15:13:52 +0200

    popravljen naslov headera

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    <title>Header dokument</title>
+    <title>New Breed Assist in digital</title>
 </head>
 <body>
 <header>
